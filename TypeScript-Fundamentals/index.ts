124ad8e446bd53baa59b0bdb2eef1a55d94fa0e3
hari14-dev <hari14code@gmail.com>
2026-02-07 13:36:46 +0530

    custom types

--- TypeScript-Fundamentals/index.ts
+++ TypeScript-Fundamentals/index.ts
@@ -1,3 +1,12 @@
+/**
+ * Challenge: Create a Pizza object type. It should include a `name`
+ * and a `price` property.
+ */
+type Pizza = {
+    name: string
+    price: number
+}
+
 const menu = [
     { name: "Margherita", price: 8 },
     { name: "Pepperoni", price: 10 },
@@ -9,11 +18,16 @@ let cashInRegister = 100
 let nextOrderId = 1
 const orderQueue = []
 
-function addNewPizza(pizzaObj) {
+/**
+ * Challenge: teach TS that the pizzaObj is supposed to be a Pizza type.
+ * Then like before, look through the code to see if there are any new
+ * TS warnings to deal with (😉), and fix those issues
+ */
+function addNewPizza(pizzaObj: Pizza) {
     menu.push(pizzaObj)
 }
 
-function placeOrder(pizzaName) {
+function placeOrder(pizzaName: string) {
     const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
     if (!selectedPizza) {
         console.error(`${pizzaName} does not exist in the menu`)
@@ -25,21 +39,15 @@ function placeOrder(pizzaName) {
     return newOrder
 }
 
-/**
- * Challenge: Teach TS what data type should be used for the 
- * orderId in the completeOrder function. Then check for any
- * additional warnings TS comes up with and fix those.
- */
-
 function completeOrder(orderId: number) {
     const order = orderQueue.find(order => order.id === orderId)
     order.status = "completed"
     return order
 }
 
-addNewPizza({ name: "Chicken Bacon Ranch", cost: 12 })
-addNewPizza({ name: "BBQ Chicken", cost: 12 })
-addNewPizza({ name: "Spicy Sausage", cost: 11 })
+addNewPizza({ name: "Chicken Bacon Ranch", price: 12 })
+addNewPizza({ name: "BBQ Chicken", price: 12 })
+addNewPizza({ name: "Spicy Sausage", price: 11 })
 
 placeOrder("Chicken Bacon Ranch")
 completeOrder(1)
--- TypeScript-Fundamentals/lesson1.ts
+++ TypeScript-Fundamentals/lesson1.ts
@@ -4,3 +4,29 @@ let myName: string = "Bob"
 
 let numberOfWheels: number = 4
 let isStudent: boolean = false
+
+// Custom Types
+// Ex1
+type Food = string
+let favoriteFood: Food = "pizza"
+
+// Ex2
+
+// we can use ',' or ';' or '' as separators in type definition
+type Person = {
+    name: string
+    age: number
+    isStudent: boolean
+}
+
+let person: Person = {
+    name: "Joe",
+    age: 42,
+    isStudent: true
+}
+
+let person2: Person = {
+    name: "Jill",
+    age: 66,
+    isStudent: false
+}
