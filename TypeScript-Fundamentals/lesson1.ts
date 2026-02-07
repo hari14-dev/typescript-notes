907141b847c5737fcb9f4cc1edd3b84c1f39bdf6
hari14-dev <hari14code@gmail.com>
2026-02-07 13:50:58 +0530

    nested object types

--- TypeScript-Fundamentals/lesson1.ts
+++ TypeScript-Fundamentals/lesson1.ts
@@ -6,27 +6,49 @@ let numberOfWheels: number = 4
 let isStudent: boolean = false
 
 // Custom Types
-// Ex1
+
 type Food = string
 let favoriteFood: Food = "pizza"
 
-// Ex2
-
 // we can use ',' or ';' or '' as separators in type definition
+
+// Nested Object Types
+
+// Challenge: try to figure out how to move the nested address object type
+// into a separate type definition. When done correctly, there should be no more
+// red errors in the editor
+
+type Address = {
+    street: string
+    city: string
+    country: string
+}
+
 type Person = {
     name: string
     age: number
     isStudent: boolean
+    address: Address
 }
 
 let person: Person = {
     name: "Joe",
     age: 42,
-    isStudent: true
+    isStudent: true,
+    address: {
+        street: "123 Main",
+        city: "Anytown",
+        country: "USA" 
+    }
 }
 
 let person2: Person = {
     name: "Jill",
     age: 66,
-    isStudent: false
+    isStudent: false,
+    address: {
+        street: "123 Main",
+        city: "Anytown",
+        country: "USA"
+    }
 }
