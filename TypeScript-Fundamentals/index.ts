78100b541d8501801bdf952058ef74ef57442c70
hari14-dev <hari14code@gmail.com>
2026-02-12 22:45:30 +0530

    use literal union type for order status and add id to pizza

--- TypeScript-Fundamentals/index.ts
+++ TypeScript-Fundamentals/index.ts
@@ -1,27 +1,24 @@
 type Pizza = {
+    id: number
     name: string
     price: number
 }
 
 /**
- * Challenge: Add an Order type. It should have `id`, `pizza`, and `status` properties.
- * Look through the code if you need a reminder as to what data types those should be.
+ * Challenge: using literal types and unions, update the Order status so that
+ * it can only ever be "ordered" or "completed"
  */
 type Order = {
     id: number
     pizza: Pizza
-    status: string
+    status: "ordered" | "completed"
 }
 
-/**
- * Challenge: Fix the TS warnings about orderQueue!
- */
-
-const menu = [
-    { name: "Margherita", price: 8 },
-    { name: "Pepperoni", price: 10 },
-    { name: "Hawaiian", price: 10 },
-    { name: "Veggie", price: 9 },
+const menu: Pizza[] = [
+    { id: 1, name: "Pepperoni", price: 10 },
+    { id: 2, name: "Margherita", price: 8 },
+    { id: 3, name: "Hawaiian", price: 10 },
+    { id: 4, name: "Veggie", price: 9 },
 ]
 
 let cashInRegister = 100
@@ -39,14 +36,10 @@ function placeOrder(pizzaName: string) {
         return
     }
     cashInRegister += selectedPizza.price
-    const newOrder = { id: nextOrderId++, pizza: selectedPizza, status: "ordered" }
+    const newOrder: Order = { id: nextOrderId++, pizza: selectedPizza, status: "ordered" }
     orderQueue.push(newOrder)
     return newOrder
 }
-
-/**
- * Challenge: Fix the warning below by handling the "sad path" scenario!
- */
 
 function completeOrder(orderId: number) {
     const order = orderQueue.find(order => order.id === orderId)
@@ -59,9 +52,9 @@ function completeOrder(orderId: number) {
     return order
 }
 
-addNewPizza({ name: "Chicken Bacon Ranch", price: 12 })
-addNewPizza({ name: "BBQ Chicken", price: 12 })
-addNewPizza({ name: "Spicy Sausage", price: 11 })
+addNewPizza({ id: 5, name: "Chicken Bacon Ranch", price: 12 })
+addNewPizza({ id: 6, name: "BBQ Chicken", price: 12 })
+addNewPizza({ id: 7, name: "Spicy Sausage", price: 11 })
 
 placeOrder("Chicken Bacon Ranch")
 completeOrder(10)
