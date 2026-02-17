04b28ab9ab8c0e045f70aefb2f6293f1b20a93be
hari14-dev <hari14code@gmail.com>
2026-02-17 20:06:22 +0530

    explicit types & function return types

--- TypeScript-Fundamentals/lesson1.ts
+++ TypeScript-Fundamentals/lesson1.ts
@@ -1,27 +1,30 @@
-// Literal Types
-
-// generic type (means, in this eg. it is string type that Ts lets myName to be any string)
-let myName = "Bob" // here the type shows as "string" (when you hover it)
-
-// literal type (Because it’s a const, TypeScript infers the string literal type "Bob", not the wider string type.)
-const myName2 = "Bob" // here the type shows as "Bob" (when you hover it)
-
-// Practice:
-let person: "Bob" = "Bobby" // this gives error, says it must be type "Bob"
-
-const person2: "Bob" = "Bob"
-
-
-// Unions
-
 type UserRole = "guest" | "member" | "admin"
 
-// let userRole: UserRole = "yttgg" // Type '"yttgg"' is not assignable to type 'UserRole'.
-let userRole: UserRole = "member"
-
-// Other egs.
 type User = {
-    userName: string
-    // role: UserRole (or)
-    role: "guest" | "member" | "admin"
+    username: string
+    role: UserRole
+}
+
+const users: User[] = [
+    { username: "john_doe", role: "member" },
+    { username: "jane_doe", role: "admin" },
+    { username: "guest_user", role: "guest" }
+];
+
+// function return type:
+// always be explicit whenever you can, here we added type User to this function, so later in the future
+// when someone tries to change the return type like 'user.username' ts warns or tells that this function is
+// supposed to return User object. 
+// This helps in avoiding unnecessary errors in our codebase, if this function is used somewhere else
+
+// other definitions:
+// Always prefer explicit return types for exported / shared functions.
+// It prevents accidental return type changes and improves maintainability.
+
+function fetchUserDetails(username: string): User {
+    const user = users.find(user => user.username === username)
+    if (!user) {
+        throw new Error(`User with username ${username} not found`)
+    }
+    return user
 }
