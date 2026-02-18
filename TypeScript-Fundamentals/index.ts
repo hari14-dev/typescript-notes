d7c3d519f68cd5f2e81f561fdd487cee107f0a53
hari14-dev <hari14code@gmail.com>
2026-02-18 13:30:40 +0530

    complete challenges

--- TypeScript-Fundamentals/index.ts
+++ TypeScript-Fundamentals/index.ts
@@ -10,25 +10,24 @@ type Order = {
     status: "ordered" | "completed"
 }
 
-const menu: Pizza[] = [
-    { id: 1, name: "Pepperoni", price: 10 },
-    { id: 2, name: "Margherita", price: 8 },
-    { id: 3, name: "Hawaiian", price: 10 },
-    { id: 4, name: "Veggie", price: 9 },
-]
-
 let cashInRegister = 100
 let nextOrderId = 1
+let nextPizzaId = 1
+
+const menu: Pizza[] = [
+    { id: nextPizzaId++, name: "Pepperoni", price: 10 },
+    { id: nextPizzaId++, name: "Margherita", price: 8 },
+    { id: nextPizzaId++, name: "Hawaiian", price: 10 },
+    { id: nextPizzaId++, name: "Veggie", price: 9 },
+]
+
 const orderQueue: Order[] = []
 
 // void type
 function addNewPizza(pizzaObj: Pizza): void {
+    pizzaObj.id = nextPizzaId++
     menu.push(pizzaObj)
 }
-
-/**
- * Challenge: add explicit return types to the rest of our functions
- */
 
 function placeOrder(pizzaName: string): Order | undefined {
     const selectedPizza = menu.find(pizzaObj => pizzaObj.name === pizzaName)
@@ -64,9 +63,31 @@ function getPizzaDetail(identifier: string | number): Pizza | undefined {
     }
 }
 
-addNewPizza({ id: 5, name: "Chicken Bacon Ranch", price: 12 })
-addNewPizza({ id: 6, name: "BBQ Chicken", price: 12 })
-addNewPizza({ id: 7, name: "Spicy Sausage", price: 11 })
+/**
+ * Challenge part 1: Make it so we can use a global variable to track the nextPizzaId
+ * and use the same trick we use with `nextOrderId++` when you're calling addNewPizza.
+ * Update the menu items to use this as well so we don't have to manually enter ids 1-4
+ * like we're currently doing
+ */
+
+// part 1 soln:
+// addNewPizza({ id: nextPizzaId++, name: "Chicken Bacon Ranch", price: 12 })
+// addNewPizza({ id: nextPizzaId++, name: "BBQ Chicken", price: 12 })
+// addNewPizza({ id: nextPizzaId++, name: "Spicy Sausage", price: 11 })
+
+/**
+ * Challenge part 1.5: Try to move the logic for adding an ID to the pizza objects 
+ * inside the addNewPizza function, so that we can call addNewPizza with no id, and
+ * the function will handle that part for us.
+ * 
+ * NOTE: you will run into TS warnings that we'll address soon, but the code should
+ * still run.
+ */
+
+// part 1.5 soln:
+addNewPizza({ name: "Chicken Bacon Ranch", price: 12 })
+addNewPizza({ name: "BBQ Chicken", price: 12 })
+addNewPizza({ name: "Spicy Sausage", price: 11 })
 
 placeOrder("Chicken Bacon Ranch")
 completeOrder(10)
